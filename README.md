df0ada0cc9eb01b70bb2b8637ed46fda8bfb206f
Илья Шалаев <alex.280200@mail.ru>
2023-05-11 17:31:01 +0300

    Сделал функцию сравнения двух файлов json, но путь возможно не любой читает, записал аксинему

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ishell90/php-project-48/workflows/hexlet-check/badge.svg)](https://github.com/ishell90/php-project-48/actions)
+[![asciicast](https://asciinema.org/a/enK9qMu4ke5RjTExsGNOdBwj3.svg)](https://asciinema.org/a/enK9qMu4ke5RjTExsGNOdBwj3)
--- src/genDiff.php
+++ src/genDiff.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace src\genDiff;
-$realFilePath1 = realpath("file1.json");
-$realFilePath2 = realpath("file2.json");
+
+//$realFilePath1 = realpath("../file1.json");
+//$realFilePath2 = realpath("../file2.json");
 
 
 function genDiff($realFilePath1, $realFilePath2)
@@ -11,11 +12,60 @@ function genDiff($realFilePath1, $realFilePath2)
     $twoStringFile = file_get_contents($realFilePath2);
     $firstDecodeFile = json_decode($firstStringFile, true);
     $twoDecodeFile = json_decode($twoStringFile, true);
-    $firstSortFile = ksort($firstDecodeFile);
-    $twoSortFile = ksort($twoDecodeFile);
+    $a = array_keys($firstDecodeFile);
+    $b = array_keys($twoDecodeFile);
+    $c = array_unique(array_merge($a, $b));
+    asort($c);
+    //ksort($firstDecodeFile);
+    //ksort($twoDecodeFile);
+    //var_dump($c);
+    $result = "";
 
-    print_r($firstSortFile);
+    foreach($c as $key1 => $value1) {
+        if(array_key_exists($value1, $firstDecodeFile) === true && array_key_exists($value1, $twoDecodeFile) === true) {
+            $meaning1 = $firstDecodeFile[$value1];
+            $meaning2 = $twoDecodeFile[$value1];
+            if($meaning1 === $meaning2) {
+                if(strlen($result) < 1) {
+                    $result = "  {$value1}: {$meaning1}";
+                } else {
+                    $result = "{$result}\n  {$value1}: {$meaning1}";
+                }
+            } else {
+                if(strlen($result) < 1) {
+                    $result = "- {$value1}: {$meaning1}\n+ {$value1}: {$meaning2}";
+                } else {
+                    $result = "{$result}\n- {$value1}: {$meaning1}\n+ {$value1}: {$meaning2}";
+                }
+            }
+        } elseif(array_key_exists($value1, $firstDecodeFile) === true && array_key_exists($value1, $twoDecodeFile) === false) {
+            if(is_string($firstDecodeFile[$value1]) === true) {
+                $meaning1 = (string) $firstDecodeFile[$value1];
+            } else {
+            $meaning1 = var_export($firstDecodeFile[$value1], true);
+            }
+            //var_dump($meaning1);
+            if(strlen($result) < 1) {
+                $result = "- {$value1}: {$meaning1}";
+            } else {
+                $result = "{$result}\n- {$value1}: {$meaning1}";
+            }
+        } else {
+            $meaning2 = var_export($twoDecodeFile[$value1], true);
+            //var_dump($meaning2);
+            if(strlen($result) < 1) {
+                $result = "+ {$value1}: {$meaning2}";
+            } else {
+                $result = "{$result}\n+ {$value1}: {$meaning2}";
+            }
+        }
+        //var_dump($result);
+    }
+    return($result);
+    
+    //var_dump($firstDecodeFile);
+    //var_dump($twoDecodeFile);
     
 }
 
-genDiff($realFilePath1, $realFilePath2);
+//genDiff($realFilePath1, $realFilePath2);
